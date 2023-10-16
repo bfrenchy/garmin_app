035e627531b8704cc924f4ce3e530f8dfb8a5d97
Ben French <ben@benfrench.co>
2023-10-16 11:33:40 -0700

    Initial transition commit

---
+++ tests/test_app.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+import streamlit as st
+
+from sifter_support_shared.functions import doc_loader, q_and_a
+# import functions.q_and_a as qs
+
+if __name__ == "__main__":
+    st.image('../../images/Garmin.png')
